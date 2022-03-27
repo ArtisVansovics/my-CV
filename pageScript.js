aa75dd29bbc291d0f940aa92cb74284ebd792207
Artis Vansovics <vansovics.artis@gmail.com>
2022-03-28 00:38:44 +0300

    Added a JS file to clean up index.html

---
+++ pageScript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
